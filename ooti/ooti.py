08455340a66dad1743c66aab18c68dd5002d32cd
vinsJ <45200439+vinsJ@users.noreply.github.com>
2021-04-29 16:18:41 +0200

    :rocket: feat - reports

--- ooti/ooti.py
+++ ooti/ooti.py
@@ -251,11 +251,16 @@ class Auth(object):
         response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response, True)
 
-    def get_invoices_sent_valid_list(self):
-        """ Get the sent and valid invoice list """
+    def get_invoices_sent_valid_list(self, team_pk):
+        """ Get the sent and valid invoice list
+
+        Keyword arguments:
+
+        team_pk -- pk of the team
+        """
 
         route = 'v1/invoices/list/{0}/?team={1}&page_size=999999&q=&is_sent=true&is_valid=true'.format(
-            self.org_pk, self.teams_pk)
+            self.org_pk, team_pk)
         response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response, True)
 
@@ -415,11 +420,16 @@ class Auth(object):
         response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response, True)
 
-    def get_credit_notes_sent_valid_list(self):
-        """ Get the sent and valid invoice list """
+    def get_credit_notes_sent_valid_list(self, team_pk):
+        """ Get the sent and valid invoice list
+
+        Keyword arguments:
+
+        team_pk -- pk of the team
+        """
 
         route = 'v1/invoices/list/{0}/?team={1}&page_size=999999&q=&is_sent=true&is_valid=true&type=9'.format(
-            self.org_pk, self.teams_pk)
+            self.org_pk, team_pk)
         response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response, True)
 
@@ -956,7 +966,7 @@ class Auth(object):
         return self.process_response(response)
 
     def delete_file(self, pk):
-        """ Delete a file 
+        """ Delete a file
 
         Keyword arguments:
 
@@ -1002,7 +1012,7 @@ class Auth(object):
                 "bic": "string",
                 "rib": "string",
                 "teams": ["string"]
-                "projects": ["string"]         
+                "projects": ["string"]
             }
         """
         route = 'v1/banks/list/{0}/'.format(self.org_pk)
@@ -1023,7 +1033,7 @@ class Auth(object):
                 "bic": "string",
                 "rib": "string",
                 "teams": ["string"]
-                "projects": ["string"]         
+                "projects": ["string"]
             }
         """
         route = 'v1/banks/{0}/'.format(pk)
@@ -1035,16 +1045,253 @@ class Auth(object):
 
         Keyword arguments:
 
-        pk -- pk of the bank 
+        pk -- pk of the bank
         """
         route = 'v1/banks/{0}/'.format(pk)
         response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    #### Reports ####
+
+    ### Reports ###
+
+    def get_reports_list(self):
+        """Get the reports list """
+
+        route = 'v1/reports/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def get_reports_project_list(self, project_pk):
+        """Get the reports list for a project"""
+
+        route = 'v1/reports/list/{0}/?page_size=999999&project={1}'.format(self.org_pk, project_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def get_report_details(self, pk):
+        """Get the report details
+
+        Keyword arguments:
+
+        pk -- pk of the report
+        """
+
+        route = 'v1/reports/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_report(self, data):
+        """ Create a report
+
+        Keyword arguments:
+
+        data -- data to be created :
+        {
+            "name": "string", (required)
+            "project": 0, (required)
+            "type": "string", (required)
+            "status": "string",
+            "styleguide": 0,
+            "lang": "string",
+            "orientation": "string",
+            "filter_team": 0,
+            "footer_team": 0,
+            "font_size": 0,
+            "margin_left": 0,
+            "margin_right": 0,
+            "cover_body_template": "string",
+            "report_body_template": "string",
+            "use_cover_page": true,
+            "year": 0,
+            "date_range": "string",
+            "is_custom_date_range": true,
+            "start_date": "string",
+            "end_date": "string",
+            "phase": 0,
+            "annex": 0,
+            "orguser": 0,
+            "scheduled_recipients": [
+                "string"
+            ],
+            "scheduled_guests": "string",
+            "scheduled_sent_at": "string",
+            "scheduled_start_date": "string",
+            "scheduled_next_date": "string",
+            "scheduled_last_sent": "string",
+            "scheduled_count": 0,
+            "scheduled_frequency": 0,
+            "hide_currency": true,
+            "notes": "string"
+        }
+
+        Note: You can create a report without "type", but this will create a blank page.
+        """
+        route = 'v1/reports/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def update_report(self, pk, data):
+        """ Update a report
+
+        Keyword arguments:
+
+        pk -- pk of the report
+        data -- data update:
+        {
+            "name": "string", (required)
+            "styleguide": 0,
+            "lang": "string",
+            "orientation": "string",
+            "footer_team": 0,
+            "font_size": 0,
+            "margin_left": 0,
+            "margin_right": 0,
+            "use_cover_page": true,
+            "scheduled_recipients": [
+                "string"
+            ],
+            "scheduled_start_date": "string",
+            "scheduled_frequency": 0,
+        }
+
+        Note: You can create a report without "type", but this will create a blank page.
+        """
+        route = 'v1/reports/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_report(self, pk):
+        """ Delete a report
+
+        Keyword arguments:
+
+        pk -- pk of the report
+        """
+        route = 'v1/reports/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def generate_report(self, data):
+        """ Generate a report (already created)
+
+        Keyword arguments:
+
+        data -- data to be created:
+            {
+                "pk": 0 (pk of the report),
+                "project": 0 (project linked to the report)
+            }
+        """
+        route = 'v1/reports/generate/{0}'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    ### Templates ###
+
+    def get_templates_list(self, team_pk):
+        """ Get list of templates
+
+        Keyword arguments:
+
+        team_pk -- pk of the team
+        """
+
+        route = 'v1/reports/templates/list/{0}/'.format(team_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def get_template_details(self, pk):
+        """Get the template details
+
+        Keyword arguments:
+
+        pk -- pk of the template
+        """
+
+        route = 'v1/reports/templates/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_template(self, team_pk, data):
+        """ Create a template
+
+        Keyword arguments:
+
+        team_pk -- pk of the team
+        data -- data create:
+            {
+                "name": "string", (required)
+                "type": "string", (required: "proprosal" or "progress")
+                "styleguide": 0,
+                "lang": "string", (required: "fr", "en", "it") 
+                "orientation": "string", (portrait, landscape)
+                "font_size": 0,
+                "margin_top": 0,
+                "margin_bottom": 0,
+                "margin_left": 0,
+                "margin_right": 0
+            }
+            """
+
+        route = 'v1/reports/templates/list/{0}/'.format(team_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def update_template(self, pk, data):
+        """ Update a template
+
+        Keyword arguments:
+
+        pk -- pk of the template
+        data -- data update:
+            {
+                "name": "string", (required)
+                "type": "string", (required: "proprosal" or "progress")
+                "styleguide": 0,
+                "lang": "string", (required: "fr", "en", "it") 
+                "orientation": "string", (portrait, landscape)
+                "font_size": 0,
+                "margin_top": 0,
+                "margin_bottom": 0,
+                "margin_left": 0,
+                "margin_right": 0
+            }
+            """
+
+        route = 'v1/reports/templates/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_template(self, pk):
+        """ Delete a template
+
+        Keyword arguments:
+
+        pk -- pk of the template
+         """
+
+        route = 'v1/reports/templates/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def duplicate_template(self, pk):
+        """ Duplicate a template
+
+        Keyword arguments:
+
+        pk -- pk of the template
+         """
+
+        route = 'v1/reports/templates/duplicate/{0}/'.format(pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
 
 
 ##### COSTS #####
 
     #### Expenses ####
+
 
     def get_expenses_list(self):
         """ Get the expenses list """
@@ -1069,6 +1316,7 @@ class Auth(object):
 ##### COLLABORATION #####
 
     #### Contact ####
+
 
     def get_contacts_list(self, project_pk=None):
         """ Get the contacts list
