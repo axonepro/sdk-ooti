37fc28a5376f2463f98b3b0d532b802f6bd6a9cd
vinsJ <45200439+vinsJ@users.noreply.github.com>
2021-05-05 15:12:30 +0200

    :rocket: feat contracts

--- ooti/deliverables.py
+++ ooti/deliverables.py
@@ -987,7 +987,6 @@ class Deliverables(Helper):
 
     def create_plan(self, project_pk, data):
         """ Create plan
-        #! Cannot test, code is required and unknown
 
         Keyword arguments:
 
@@ -1007,10 +1006,11 @@ class Deliverables(Helper):
                 "plan_code": "string",
                 "project": 0,
                 "area": 0,
-                "code": "string",
+                "code": "string", ("pln", "det", "elv", "sec")
                 "custom_field_1": "string",
                 "custom_field_2": "string",
-                "custom_field_3": "string"
+                "custom_field_3": "string",
+                "org": 0
             }
         """
 
@@ -1020,7 +1020,6 @@ class Deliverables(Helper):
 
     def get_plan_details(self, plan_pk):
         """ Get plans details
-        #! Cannot test, cannot create plan
 
         Keyword arguments:
 
@@ -1033,7 +1032,6 @@ class Deliverables(Helper):
 
     def update_plan(self, plan_pk, data):
         """ Update plan
-        #! Cannot test, cannot create plan
 
         Keyword arguments:
 
@@ -1066,7 +1064,7 @@ class Deliverables(Helper):
 
     def delete_plan(self, plan_pk):
         """ Delete plan
-        #! Cannot test, cannot create plan
+
         Keyword arguments:
 
         plan_pk -- the pk of the plan
@@ -1781,6 +1779,393 @@ class Deliverables(Helper):
         """
 
         route = 'v1/documents/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    #### Contracts ####
+
+    ### Contractors ###
+    def get_contractors_list(self):
+        """ Get contractors list """
+
+        route = 'v1/contracts/contractor/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_contractors(self, data):
+        """ Create a contractor
+
+        Keyword arguments:
+
+        data -- data create : 
+            {
+                "identity_team": 0,
+                "currency": 0,
+                "name": "string",
+                "photo": "string",
+                "email": "string",
+                "address": "string",
+                "telephone": "string",
+                "iban": "string",
+                "number": "string",
+                "is_company": true,
+                "is_internal": true,
+                "tags": [
+                "string"
+                ]
+            }
+        """
+
+        route = 'v1/contracts/contractor/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_contractor_details(self, pk):
+        """ Get contractor details
+
+        Keyword arguments:
+
+        pk -- the pk of the contractor
+        """
+
+        route = 'v1/contracts/contractor/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_contractor(self, pk, data):
+        """ Update a contractor
+
+        Keyword arguments:
+
+        pk -- the pk of the contractor
+        data -- data create : 
+            {
+                "identity_team": 0,
+                "currency": 0,
+                "name": "string",
+                "photo": "string",
+                "email": "string",
+                "address": "string",
+                "telephone": "string",
+                "iban": "string",
+                "number": "string",
+                "is_company": true,
+                "is_internal": true,
+                "tags": [
+                "string"
+                ]
+            }
+        """
+
+        route = 'v1/contracts/contractor/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_contractor(self, pk):
+        """ Delete contractor
+
+        Keyword arguments:
+
+        pk -- the pk of the contractor
+        """
+
+        route = 'v1/contracts/contractor/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    ### Contract items ###
+    def generate_contracts_project(self, project_pk):
+        """ Copy contracts from fee projects to annexes
+
+        Keyword arguments:
+
+        project_pk -- pk of the project :
+        """
+
+        route = 'v1/contracts/generate/contracts/{0}/'.format(project_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def generate_contracts_org(self):
+        """ Generate contracts """
+
+        route = 'v1/contracts/generate/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_contracts_items_list(self):
+        """ Get contracts item list """
+
+        route = 'v1/contracts/item/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_contracts_items(self, data):
+        """ Create a contract item
+        #! Cannot test because cannot create contract
+        Keyword arguments:
+
+        data -- data create : 
+            {
+                "contract": 0,
+                "phase": 0,
+                "annex": 0,
+                "type": "string",
+                "fee": 0,
+                "amount_current": 0,
+                "already_billed": 0,
+                "amount_billed": 0,
+                "already_paid": 0
+            }
+        """
+
+        route = 'v1/contracts/item/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_contract_item_details(self, pk):
+        """ Get contract item details
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the item
+        """
+
+        route = 'v1/contracts/item/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_contract_item(self, pk, data):
+        """ Update a contract item
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- pk of the item
+        data -- data update : 
+            {
+                "contract": 0,
+                "phase": 0,
+                "annex": 0,
+                "type": "string",
+                "fee": 0,
+                "amount_current": 0,
+                "already_billed": 0,
+                "amount_billed": 0,
+                "already_paid": 0
+            }
+        """
+
+        route = 'v1/contracts/item/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_contract_item(self, pk):
+        """ Delete a contract item
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- pk of the item
+        """
+
+        route = 'v1/contracts/item/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    ### Contracts ###
+    def get_contracts_list(self):
+        """ Get contracts list """
+
+        route = 'v1/contracts/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_contract(self, data):
+        """ Create a contract
+        #! A contract can be added to a fee_project under review only. 
+        #! Cannot test because cannot get fee_project based on a project 
+
+        Keyword arguments:
+
+        data -- data create : 
+            {
+                "contractor": 0, (R)
+                "manager": 0,
+                "fee_project": 0,
+                "is_valid": true,
+                "is_archived": true,
+                "type": "string", (R) ("sub")
+                "forecast_type": "string",
+                "invoicing_method": "string",
+                "description": "string",
+                "is_mandate": true,
+                "tax_rate": 0,
+                "project": 0,
+                "fee_project": 0
+            }
+        """
+
+        route = 'v1/contracts/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_contract_details(self, pk):
+        """ Get contract details
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the contract
+        """
+
+        route = 'v1/contracts/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_contract(self, pk, data):
+        """ Update a contract
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the contract
+        data -- data create : 
+            {
+                "contractor": 0,
+                "manager": 0,
+                "fee_project": 0,
+                "is_valid": true,
+                "is_archived": true,
+                "type": "string",
+                "forecast_type": "string",
+                "invoicing_method": "string",
+                "description": "string",
+                "is_mandate": true,
+                "tax_rate": 0
+            }
+        """
+
+        route = 'v1/contracts/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_contract(self, pk):
+        """ Delete contract 
+        #! Cannot test because cannot create contract
+        Keyword arguments:
+
+        pk -- the pk of the contract
+        """
+
+        route = 'v1/contracts/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    ### Contract month ###
+    def generate_contracts_month_org(self, contract_pk):
+        """ Generate contract month 
+        #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        contract_pk -- pk of the contract
+        """
+
+        data = {
+            "contract": contract_pk
+        }
+
+        route = 'v1/contracts/month/generate/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_contracts_month_list(self):
+        """ Get contracts list """
+
+        route = 'v1/contracts/month/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_contracts_month(self, data):
+        """ Create a contract month
+         #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        data -- data create : 
+            {
+                "contract": 0,
+                "year": 0,
+                "month": 0,
+                "start_date": "string",
+                "end_date": "string",
+                "is_past": true,
+                "is_present": true,
+                "is_future": true,
+                "budget": 0,
+                "budget_projected": 0,
+                "actual": 0,
+                "pct": 0
+            }
+        """
+
+        route = 'v1/contracts/month/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_contract_month_details(self, pk):
+        """ Get contract details
+         #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the contract month
+        """
+
+        route = 'v1/contracts/month/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_contracts_month(self, pk, data):
+        """ Create a contract month
+         #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the contract month
+        data -- data create : 
+            {
+                "contract": 0,
+                "year": 0,
+                "month": 0,
+                "start_date": "string",
+                "end_date": "string",
+                "is_past": true,
+                "is_present": true,
+                "is_future": true,
+                "budget": 0,
+                "budget_projected": 0,
+                "actual": 0,
+                "pct": 0
+            }
+        """
+
+        route = 'v1/contracts/month/{0}/'.format(pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_contract_month(self, pk):
+        """ Delete contract month
+         #! Cannot test because cannot create contract
+
+        Keyword arguments:
+
+        pk -- the pk of the contract month
+        """
+
+        route = 'v1/contracts/month/{0}/'.format(pk)
         response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
 
