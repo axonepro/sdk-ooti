a7ca22dbeadc09279e639058671e0134de484c46
Thomas <79807975+Tsotssi@users.noreply.github.com>
2021-05-06 12:20:41 +0200

    Adding employees methods

--- ooti/costs.py
+++ ooti/costs.py
@@ -17,6 +17,196 @@ class Costs(Helper):
 
     #### Employees ####
 
+    def get_employees_contracts_list(self):
+        """ Get the employees contracts """
+
+        route = 'v1/employees/contracts/list/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_employees_contract(self, data):
+        """ Create a new employees 
+
+        Keywords arguments:
+        data -- data of the new contract to be created:
+        {
+            "team": team_pk,
+            "orguser": orguser_pk,
+            "status": "string",
+            "start_date": "string",
+            "end_date": "string",
+            "cancel_date": "string",
+            "notes": "string",
+            "currency": currency_pk,
+            "pay_type": "string",
+            "admin_only": true,
+            "contract_type": "string",
+            "salary_gross_coefficent": 0,
+            "salary_monthly_net": 0,
+            "weekly_hours_limit": 0,
+            "daily_hours_limit": 0,
+            "overtime_enabled": true,
+            "overtime_hours_limit": 0,
+            "days_per_week": 0,
+            "file": "string",
+            "salary_monthly_gross": 0,
+            "salary_hourly_gross": 0,
+            "salary_loaded_coefficent": 0,
+            "salary_daily_gross": 0
+        }
+        """
+
+        route = 'v1/employees/contracts/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_employees_contracts_details(self, contract_id):
+        """ Get employees contract details 
+
+        Keywords arguments:
+        contract_id -- id of the employees contract
+        """
+
+        route = 'v1/employees/contracts/{0}/'.format(contract_id)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_employees_contracts_details(self, contract_id, data):
+        """ Get employees contract details 
+
+        Keywords arguments:
+        contract_id -- id of the employees contract
+        data -- content of the update:
+        {
+            "team": team_pk,
+            "orguser": orguser_pk,
+            "status": "string",
+            "start_date": "string",
+            "end_date": "string",
+            "cancel_date": "string",
+            "notes": "string",
+            "currency": currency_pk,
+            "pay_type": "string",
+            "admin_only": true,
+            "contract_type": "string",
+            "salary_gross_coefficent": 0,
+            "salary_monthly_net": 0,
+            "weekly_hours_limit": 0,
+            "daily_hours_limit": 0,
+            "overtime_enabled": true,
+            "overtime_hours_limit": 0,
+            "days_per_week": 0,
+            "file": "string",
+            "salary_monthly_gross": 0,
+            "salary_hourly_gross": 0,
+            "salary_loaded_coefficent": 0,
+            "salary_daily_gross": 0
+        }
+        """
+
+        route = 'v1/employees/contracts/{0}/'.format(contract_id)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_employees_contracts(self, contract_id):
+        """ Delete employees contract
+
+        Keywords arguments:
+        contract_id -- id of the employees contract
+        """
+
+        route = 'v1/employees/contracts/{0}/'.format(contract_id)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_employees_period_list(self):
+        """ Get the list of employees periods """
+
+        route = 'v1/employees/period/list/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_employees_period(self, data):
+        """ Create a new employees period
+
+        Keywords arguments:
+        data -- data of the new period to be created:
+        {
+            "contract": contract_id,
+            "notes": "string",
+            "start_date": "string",
+            "end_date": "string",
+            "status": "string",
+            "salary_daily_gross": 0,
+            "salary_hourly_gross": 0,
+            "salary_gross_coefficent": 0,
+            "salary_monthly_net": 0,
+            "salary_monthly_gross": 0,
+            "salary_loaded_coefficent": 0,
+            "weekly_hours_limit": 0,
+            "daily_hours_limit": 0,
+            "overtime_enabled": true,
+            "overtime_hours_limit": 0,
+            "days_per_week": 0
+        }
+        """
+
+        route = 'v1/employees/period/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_employees_period_details(self, period_id):
+        """ Get employees period details
+
+        Keywords arguments:
+        period_id -- id of the period
+        """
+
+        route = 'v1/employees/period/{0}/'.format(period_id)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_employees_period_details(self, period_id, data):
+        """ Update the employees period details 
+
+        Keywords arguments:
+        period_id -- id of the period
+        data -- content of the update:
+        {
+            "contract": contract_id,
+            "notes": "string",
+            "start_date": "string",
+            "end_date": "string",
+            "status": "string",
+            "salary_daily_gross": 0,
+            "salary_hourly_gross": 0,
+            "salary_gross_coefficent": 0,
+            "salary_monthly_net": 0,
+            "salary_monthly_gross": 0,
+            "salary_loaded_coefficent": 0,
+            "weekly_hours_limit": 0,
+            "daily_hours_limit": 0,
+            "overtime_enabled": true,
+            "overtime_hours_limit": 0,
+            "days_per_week": 0
+        }
+        """
+
+        route = 'v1/employees/period/{0}/'.format(period_id)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_employees_period(self, period_id):
+        """ Delete the employees period
+
+        Keywords arguments:
+        period_id -- id of the period
+        """
+
+        route = 'v1/employees/period/{0}/'.format(period_id)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
     #### Expenses ####
 
     # see questions below
