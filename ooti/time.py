1edcf64c26ad59fcd56aee8f2c388292a135bd3c
vinsJ <45200439+vinsJ@users.noreply.github.com>
2021-05-11 14:21:53 +0200

    :rocket: feat - timeperiods

--- ooti/time.py
+++ ooti/time.py
@@ -782,7 +782,7 @@ class Time(Helper):
         """
 
         route = 'v1/timelogs/weeks/list/{0}/'.format(self.org_pk)
-        parameters = "?start_date={0}".format(start_date)
+        parameters = "?start_date={0}&page_size=999999=".format(start_date)
 
         response = requests.get('{0}{1}{2}'.format(self.base_url, route, parameters), headers=self.headers)
         return self.process_response(response, True)
@@ -929,4 +929,264 @@ class Time(Helper):
 
         route = 'v1/timelogs/worklogs/{0}/'.format(pk)
         response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    #### Timeperiods ####
+    def get_timeperiods_dashboard_scheduling_timeline(self):
+        """ Get scheduling timeline """
+
+        route = 'v1/timeperiods/dashboard/scheduling/timeline/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_timeperiods_resource_planning_timeline(self):
+        """ Get planning timeline """
+
+        route = 'v1/timeperiods/resource-planning-timeline/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_timeperiods_resource_planning_timeline(self):
+        #! 403, not have permission
+        """ Create planning timeline """
+
+        route = 'v1/timeperiods/resource-planning-timeline/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_timeperiods_resources_timeline(self, project_pk):
+        """ Get scheduling timeline 
+
+        Keyword arguments: 
+
+        project_pk -- pk of the project
+        """
+
+        route = 'v1/timeperiods/resources-timeline/{0}/'.format(project_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_timeperiods_role_annex_periods(self):
+        #! 403, not have permission
+        """ Get week config list """
+
+        route = 'v1/timeperiods/role-annex-periods/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_timeperiods_role_annex_periods(self, data):
+        """ Get week config list 
+
+        Keyword arguments:
+
+        data -- data create: 
+        {
+            "start_date": "string",
+            "end_date": "string",
+            "hours_per_day": 0,
+            "personnel_count": 0,
+            "include_saturday": true,
+            "include_sunday": true,
+            "hours_scheduled": 0,
+            "hours_scheduled_per_user": 0,
+            "annex": 0, (R) 
+            "role": 0,
+            "project_role": 0,
+            "orgusers": [
+                "string"
+            ],
+            "budget_pct": 0
+        }
+
+        """
+
+        route = 'v1/timeperiods/role-annex-periods/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_timeperiods_role_annex_period_details(self, pk):
+        """ Get role annex period details  
+
+        Keyword arguments:
+
+        pk -- pk of the role annex period
+        """
+
+        route = 'v1/timeperiods/role-annex-periods/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_timeperiods_role_annex_period(self, pk, data):
+        """ Update role annex period details  
+
+        Keyword arguments:
+
+        pk -- pk of the role annex period
+        data -- data update :
+        {
+            "start_date": "string",
+            "end_date": "string",
+            "hours_per_day": 0,
+            "personnel_count": 0,
+            "include_saturday": true,
+            "include_sunday": true,
+            "hours_scheduled": 0,
+            "hours_scheduled_per_user": 0,
+            "annex": 0,
+            "role": 0,
+            "project_role": 0,
+            "orgusers": [
+                "string"
+            ],
+            "budget_pct": 0
+        }
+        """
+
+        route = 'v1/timeperiods/role-annex-periods/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_timeperiods_role_annex_period(self, pk):
+        """ Delete role annex period  
+
+        Keyword arguments:
+
+        pk -- pk of the role annex period
+        """
+
+        route = 'v1/timeperiods/role-annex-periods/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_timeperiods_scheduling_timeline_actions(self, project_pk):
+        #! 200, no body response
+        """
+        Get scheduling timeline actions for a project
+
+        Keyword arguments:
+
+        pk -- pk of the project
+        """
+
+        route = 'v1/timeperiods/scheduling-timeline/actions/{0}/'.format(project_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_timeperiods_scheduling_timeline_actions(self, project_pk):
+        """
+        Get scheduling timeline actions for a project
+
+        Keyword arguments:
+
+        pk -- pk of the project
+        """
+
+        route = 'v1/timeperiods/scheduling-timeline/{0}/'.format(project_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_user_period_action(self):
+        #! 200, no body response
+        """ Create user period action """
+
+        route = 'v1/timeperiods/user-period/list/action/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_user_period_list(self):
+        """ Get user period list"""
+
+        route = 'v1/timeperiods/user-period/list/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_user_period_list(self, data):
+        """ Get user period
+
+        Keyword argument:
+
+        data -- data create:
+        {
+            "orguser": 0,
+            "role": 0,
+            "time_type": 0,
+            "team": 0,
+            "project": 0,
+            "phase": 0,
+            "annex": 0,
+            "start_date": "string",
+            "end_date": "string",
+            "hours_per_day": 0,
+            "hours": 0,
+            "pct_of_day": 0,
+            "description": "string",
+            "sync_with_budget": true,
+            "sync_with_planning": true
+        }
+        """
+
+        route = 'v1/timeperiods/user-period/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_user_period_details(self, pk):
+        #! 403
+        """ Get user period details
+
+        Keyword arguments:
+
+        pk -- pk of the user perdio
+        """
+
+        route = 'v1/timeperiods/user-period/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_user_period_list(self, pk, data):
+        """ Update user period
+
+        Keyword argument:
+
+        pk -- pk of the user period
+        data -- data update:
+        {
+            "orguser": 0,
+            "role": 0,
+            "time_type": 0,
+            "team": 0,
+            "project": 0,
+            "phase": 0,
+            "annex": 0,
+            "start_date": "string",
+            "end_date": "string",
+            "hours_per_day": 0,
+            "hours": 0,
+            "pct_of_day": 0,
+            "description": "string",
+            "sync_with_budget": true,
+            "sync_with_planning": true
+        }
+        """
+
+        route = 'v1/timeperiods/user-period/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_user_period(self, pk):
+        """ Get user period details
+
+        Keyword arguments:
+
+        pk -- pk of the user perdio
+        """
+
+        route = 'v1/timeperiods/user-period/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_users_scheduling_timeline(self):
+        """ Get user scheduling timeline """
+
+        route = 'v1/timeperiods/users/scheduling-timeline/{0}/'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
