06b344982b011dc1356544e40ff2e5b12baebe90
vinsJ <45200439+vinsJ@users.noreply.github.com>
2021-05-11 15:52:44 +0200

    :rocket: feat - roles

--- ooti/time.py
+++ ooti/time.py
@@ -1202,3 +1202,156 @@ class Time(Helper):
         route = 'v1/timeperiods/users/scheduling-timeline/{0}/'.format(self.org_pk)
         response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
+
+    #### Roles ####
+    def get_roles_list(self):
+        """ Get roles list """
+
+        route = 'v1/roles/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_roles(self, data):
+        """ Get roles list 
+
+        Keyword arguments:
+
+        data -- data create:
+        {
+            "title": "string",
+            "billable_per_hour": 0,
+            "payable_per_hour": 0
+        }
+        """
+
+        route = 'v1/roles/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_roles_project_list(self):
+        """ Get roles project list """
+
+        route = 'v1/roles/project/list/{0}/?page_size=999999'.format(self.org_pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response, True)
+
+    def create_roles_project(self, data):
+        """ Get roles project list 
+
+        Keyword arguments:
+
+        data -- data create:
+        {
+            "project": 0,
+            "role": 0,
+            "billable_per_hour": 0,
+            "payable_per_hour": 0,
+            "pay_per_hour": 0,
+            "personnel_count": 0
+        }
+        """
+
+        route = 'v1/roles/project/list/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def get_roles_project_details(self, pk):
+        """ Get roles details 
+
+        Keyword arguments:
+
+        pk -- pk of the roles project
+        """
+
+        route = 'v1/roles/project/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_roles_project(self, pk, data):
+        """ Update roles project 
+
+        Keyword arguments:
+
+        pk -- pk of the roles project
+        data -- data create:
+        {
+            "project": 0,
+            "role": 0,
+            "billable_per_hour": 0,
+            "payable_per_hour": 0,
+            "pay_per_hour": 0,
+            "personnel_count": 0
+        }
+        """
+
+        route = 'v1/roles/project/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_roles_project(self, pk):
+        """ Delete roles project 
+
+        Keyword arguments:
+
+        pk -- pk of the roles project
+        """
+
+        route = 'v1/roles/project/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def create_bulk_action_add_roles(self):
+        """ Create bulk action to add role to project """
+
+        route = 'v1/roles/roles/bulk/add/{0}/'.format(self.org_pk)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def delete_bulk_action_add_roles(self):
+        """ Delete bulk action to add role to project """
+
+        route = 'v1/roles/roles/bulk/delete/{0}/'.format(self.org_pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def get_roles_details(self, pk):
+        """ Get roles details 
+
+        Keyword arguments:
+
+        pk -- pk of the roles project
+        """
+
+        route = 'v1/roles/{0}/'.format(pk)
+        response = requests.get('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
+
+    def update_roles(self, pk, data):
+        """ Update roles 
+
+        Keyword arguments:
+
+        pk -- pk of the roles
+        data -- data create:
+        {
+            "title": "string",
+            "billable_per_hour": 0,
+            "payable_per_hour": 0
+        }
+        """
+
+        route = 'v1/roles/{0}/'.format(pk)
+        response = requests.patch('{0}{1}'.format(self.base_url, route), headers=self.headers, data=json.dumps(data))
+        return self.process_response(response)
+
+    def delete_roles(self, pk):
+        """ Delete roles 
+
+        Keyword arguments:
+
+        pk -- pk of the roles 
+        """
+
+        route = 'v1/roles/{0}/'.format(pk)
+        response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        return self.process_response(response)
