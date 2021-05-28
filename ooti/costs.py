50205313893a8931e5c77b7d8512a5ebbf3ace1c
Thomas <79807975+Tsotssi@users.noreply.github.com>
2021-05-28 11:51:46 +0200

    Adding missing docstrings

--- ooti/costs.py
+++ ooti/costs.py
@@ -32,19 +32,45 @@ class Costs(Helper):
 
     # copy & set funcions ?
 
-    def copy_costs_fee_allocations_from_contract_hours(self, project_id):  # ?
+    def copy_costs_fee_allocations_from_contract_hours(self, project_id):
+        """ Copy cost fee allocations from contract hours
+
+        Keywords arguments:
+        project_id -- id of the project
+        data -- fee project to be copied
+        {
+            "fee_project": fee_project_pk
+        }
+        """
 
         route = 'v1/costs/copy-fee-allocations-from-contract-hours/{0}/'.format(project_id)
         response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
 
-    def copy_costs_fee_allocations_from_subcontractor_fees(self, project_id):  # ?
+    def copy_costs_fee_allocations_from_subcontractor_fees(self, project_id):
+        """ Copy cost fee allocations from subcontractor fees
+
+        Keywords arguments:
+        project_id -- id of the project
+        data -- fee project to be copied
+        {
+            "fee_project": fee_project_pk
+        }
+        """
 
         route = 'v1/costs/copy-fee-allocations-from-subcontractor-fees/{0}/'.format(project_id)
         response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
 
-    def copy_costs_previous_year(self):  # ?
+    def copy_costs_previous_year(self):
+        """ Copy costs from a previous year
+
+        Keywords arguments:
+        data -- year of the costs to be copied
+        {
+            "year": 0
+        }
+        """
 
         route = 'v1/costs/copy-prev-year/{0}/'.format(self.org_pk)
         response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
@@ -118,10 +144,18 @@ class Costs(Helper):
         response = requests.delete('{0}{1}'.format(self.base_url, route), headers=self.headers)
         return self.process_response(response)
 
-    def set_costs_annual_budget(self, cost_id):  # ?
+    def set_costs_annual_budget(self, cost_id, data):
+        """ Set the cost annual budget
 
+        Keywords arguments:
+        cost_id -- id of the cost
+        data -- annual amount budgeted to be set:
+        {
+            "amount_budgeted": 0
+        }
+        """
         route = 'v1/costs/set-annual-budget/{0}/'.format(cost_id)
-        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers)
+        response = requests.post('{0}{1}'.format(self.base_url, route), headers=self.headers, data=data)
         return self.process_response(response)
 
     def get_costs_list(self):
