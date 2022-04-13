b9da55fc9a44165f315bb058516203448f645b49
GuillaumeC <guillaumec@MBPdeGuillaumeC.home>
2022-04-13 18:06:17 +0200

    change on invoice use case

--- examples/use_case_invoice.py
+++ examples/use_case_invoice.py
@@ -8,12 +8,7 @@ def create_invoice_for_project():
     my_account = ooti.OotiAPI(config['EMAIL'], config['PASSWORD'])  #connect to ooti
     my_account.connect()
 
-    proj=my_account.Projects.get_projects_list()
-    id=proj['data'][0]['id']
-    phases=my_account.Phases.get_phases_list(id)
-    pprint.pp(phases)
-
-    """team_pk=my_account.Teams.get_teams_list()['data'][0]['pk']
+    team_pk=my_account.Teams.get_teams_list()['data'][0]['pk']
     client_pk=my_account.Clients.get_clients_list(team_pk)['data'][0]['pk']
     currency_pk=my_account.Currencies.get_currencies_list()['data'][0]['pk']
 
@@ -24,12 +19,22 @@ def create_invoice_for_project():
     'start_date':'28-04-2020',
     'end_date':'28-08-2020',
     'city':'Deauville',
-    'country':'FR'
+    'country':'FR',
+    'total_fees':100
     }
 
 
 
-    my_account.Projects.create_project(data)"""
+    my_account.Projects.create_project(data)
+
+    projects=my_account.Projects.get_projects_list()
+    for i in projects['data']:
+        if(i['project_title']=='test_project'):
+            id=i['id']
+    
+    my_account.Phases.create_phase(id,{'name':'phase1','pct':10.0})
+    my_account.Phases.create_phase(id,{'name':'phase2','pct':60.0})
+    my_account.Phases.create_phase(id,{'name':'phase3','pct':30.0})
 
 create_invoice_for_project()
 
