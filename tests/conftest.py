efe7d0e0792b8ac4408c9d63c0db7f8ffd9ab8d2
Thomas Kgaevski <thomas.kgaevski@gmail.com>
2023-02-03 12:43:07 +0100

    Add parent dir to python path in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import os
+import sys
 
 import pytest
 from dotenv import load_dotenv
+
+PACKAGE_PARENT = ".."
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from resources import ooti  # noqa E402
 
